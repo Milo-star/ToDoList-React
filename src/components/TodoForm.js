5877a082a96075b62efd64f9b6cab3ee8a768c07
Milo Bonnet <milovtt@gmail.com>
2023-05-23 12:10:24 +0200

    Add message error

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,22 +1,33 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
-export const TodoForm = ({addTodo}) => {
-    const [value, setValue] = useState("")
+export const TodoForm = ({ addTodo }) => {
+    const [value, setValue] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        addTodo(value)
-
-        setValue("")
-    }
+        if (value.trim() === "") {
+            setErrorMessage("Veuillez saisir un message.");
+        } else {
+            addTodo(value);
+            setValue("");
+            setErrorMessage("");
+        }
+    };
 
     return (
         <form className='TodoForm' onSubmit={handleSubmit}>
-            <input type='text' className='todo-input' value={value}
-            placeholder='What is the tash today ?' onChange={(e) => setValue(e.target.value)}/>
+            <input
+                type='text'
+                className='todo-input'
+                value={value}
+                placeholder='What is the task today ?'
+                onChange={(e) => setValue(e.target.value)}
+            />
 
             <button type='submit' className='todo-btn'>Add Task</button>
+            {errorMessage && <p className="error-message">{errorMessage}</p>}
         </form>
-    )
-}
+    );
+};
